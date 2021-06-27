d59a47b725a3582f3f84acd043ea0246aa2b53a0
Yash Prakash <yash2002109@gmail.com>
2021-06-27 21:39:20 +0530

    Shift automation system from dev branch to main  (#3)
    * Add skeleton report MARS
    
    Signed-off-by: Yash Prakash <yash2002109@gmail.com>
    
    * Organize files & add support to generate FA tables from YAML
    
    Signed-off-by: Yash Prakash <yash2002109@gmail.com>
    
    * created README for yml config file
    
    Signed-off-by: ritik-malik <ritik18406@iiitd.ac.in>
    
    * fixed typos
    
    Signed-off-by: ritik-malik <ritik18406@iiitd.ac.in>
    
    * renamed yml file and fixed flags in yml and code
    
    Signed-off-by: ritik-malik <ritik18406@iiitd.ac.in>
    
    * fixed extraction of metrics and questions
    
    Signed-off-by: ritik-malik <ritik18406@iiitd.ac.in>
    
    * Remove extra code & rerun scripts
    
    Signed-off-by: Yash Prakash <yash2002109@gmail.com>
    
    * Add wg-evolution to YML configuration file
    
    Signed-off-by: Yash Prakash <yash2002109@gmail.com>
    
    Co-authored-by: maalik <ritik18406@iiitd.ac.in>

---
+++ automation-english/helper.py
@@ -0,0 +1,83 @@
+from string import punctuation
+
+template_start = r'''
+\subsection{Focus Area - $FOCUS_AREA_NAME$}
+\textbf{Goal:} $FOCUS_AREA_GOAL$
+\begin{table}[ht!]
+    \centering
+    \begin{tabular}{|p{0.35\linewidth} | p{0.6\linewidth}|}
+        \hline
+        \hfil \textbf{Metric}  & \hfil \textbf{Question} \\
+        \hline
+'''
+
+template_end = r'''    \end{tabular}
+\end{table}
+'''
+
+def extract_goal(focus_area_README):
+
+    with open(focus_area_README) as f:
+        data = f.readlines()
+    data = [x.strip() for x in data]
+
+    # filter out empty strings
+    data = list(filter(None, data))
+
+    # data[1] = '**Goal:** The goal to be extracted...'
+    # 1) Split into list
+    # 2) Remove the first word
+    # 3) Join the list and return
+    return ' '.join(data[1].split()[1:])
+
+
+def extract_question(metric):
+
+    with open(metric) as f:
+        data = f.readlines()
+    data = [x.strip() for x in data]
+
+    # filter out empty strings
+    data = list(filter(None, data))
+
+    # data[0] = '# Technical Fork'
+    metric_name = ' '.join(data[0].split()[1:])
+
+    # data[1] = 'Question: question part of the metric'
+    metric_question = ' '.join(data[1].split()[1:])
+
+    return metric_name, metric_question
+
+
+def generate_focus_areas(focus_area_name, focus_area_README, metrics):
+
+    table_head = template_start
+    table_tail = template_end
+
+    focus_area_goal = extract_goal(focus_area_README)
+
+    table_head = table_head.replace("$FOCUS_AREA_NAME$", focus_area_name.title())
+    table_head = table_head.replace("$FOCUS_AREA_GOAL$", focus_area_goal)
+
+    for metric in metrics:
+        metric_name, metric_question = extract_question(metric)
+        table_head += '\t\t' + metric_name + ' & ' + metric_question + ' \\\\ \n\t\t\hline\n'
+
+    table_head += table_tail
+
+    ## TODO: add the name of metrics files
+    # \input{techical-fork} 
+    # .... metrics...
+
+    file_name = focus_area_name + '.tex'
+    with open(file_name, 'w') as f:
+        f.write(table_head)
+
+    print(f"Written data to file = {file_name}")
+
+
+
+
+
+
+
---
+++ automation-english/inessentials/wg-value/focus-areas/social-value/README.md
@@ -0,0 +1,7 @@
+# Social Value
+
+**Goal:** Identify if a project reduces poverty and injustice, strengthens democratic values, improves health and mental wellbeing, promotes international cooperation, fosters economic equality, or advances human achievement.
+
+Name | Question
+--- | ---
+TBD | TBD
