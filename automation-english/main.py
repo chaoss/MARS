c0d84b14b757b4aaae0942988a0aa9295928036a
Yash Prakash <yash2002109@gmail.com>
2021-07-29 22:48:49 +0530

    Add support for translations (Chinese)
    Signed-off-by: Yash Prakash <yash2002109@gmail.com>

--- automation-english/main.py
+++ automation-english/main.py
@@ -2,6 +2,7 @@ import os
 # import shutil
 import sys
 import english_release
+import translations_release
 import helper
 
 ##### Global Vars #####
@@ -9,6 +10,7 @@ import helper
 english_yml_filename = "english_working-groups-config.yml"
 spanish_yml_filename = "spanish_working-groups-config.yml"
 chinese_yml_filename = "chinese_working-groups-config.yml"
+translations = {"github-link": "https://github.com/chaoss/translations", "github-branch": "main", "repo-name": "translations" }
 master_file_path = "master.tex"
 test_dir = "test_env"
 current_dir = "./"
@@ -41,6 +43,9 @@ def main():
     global test_dir
     global current_dir
 
+    # select language
+    language = language_input()
+    
     # Clean the test_dir for residual files
     helper.clean_directory(test_dir)
 
@@ -52,12 +57,10 @@ def main():
     
     print(f"\nSwitching to {test_dir}")
 
+
     # Copy the scripts
     helper.copy_dir_files("../active_user_input", current_dir)
     helper.copy_dir_files("../passive_user_input", current_dir)
-
-    # select language
-    language = language_input()
 
     if language == '1':
         print("English selected!")
@@ -67,6 +70,14 @@ def main():
         english_release.english_main(english_yml_filename)
 
     else:
+        print("Translations selected!")
+        helper.clone_repo(translations["github-link"], translations["repo-name"],translations["github-branch"])
+        detected_languages = [dir for dir in sorted(os.listdir(translations["repo-name"])) if os.path.isdir(os.path.join(translations["repo-name"], dir)) and dir[0] != "."]
+
+        for i in range(len(detected_languages)):
+            print(f"PRESS {i} for {detected_languages[i]}")
+        user_inp = int(input("Your preference: "))
+        translations_release.translations_main(detected_languages[user_inp])
         print("Work in progress")
     sys.exit()
 
